456d0e8447b088a701c8804a70b034389ab860c5
Wajiha Khan <jiya.r.kh@gmail.com>
2024-05-16 20:48:31 -0400

    hard coded list of things to do, currently fixing completed status

--- server/task.js
+++ server/task.js
@@ -6,17 +6,55 @@ const chance = new Chance();
 class Task {
   constructor() {
     this.tasks = [];
+    this.todoList = [
+      'Exercise for 30 minutes',
+      'Organize closet',
+      'Reply to all email',
+      'Buy pet food',
+      'Walk the dog',
+      'Pay bills',
+      'Set up automatic bill payment',
+      'Backup important files to an external drive',
+      'Take out the trash and recycling',
+      'Buy groceries for the weekend',
+      'Prepare meal plan for the week',
+      'Clean kitchen thoroughly',
+      'Water indoor plants',
+      'Call the bank about the credit card issue',
+      'Schedule doctor appointments',
+      'Clean out the garage',
+      'Dust the shelves and furniture',
+      'Change bed sheets',
+      'Write on a birthday card for Tom and buy gift',
+      'Replace the air filters',
+      'Update insurance policies',
+      'Clean all windows',
+      'Set up a meeting with the team',
+      'Pick up the dry cleaning',
+      'Review and edit photos',
+      'Print and frame photos',
+      'Plan date night',
+      'Plan family game night',
+      'Plan weekend trip with friends',
+      'Create a vision board for trip',
+    ];
   }
 
   createTask() {
     const task = {
       id: chance.guid(),
-      description: chance.sentence({ words: 5 }),
+      reminders: this.getRandomTodo(),
       status: 'pending',
       dueDate: chance.date({ string: true }),
+      complete: false,
     };
     this.tasks.push(task);
     return task;
+  }
+
+  getRandomTodo() {
+    const index = Math.floor(Math.random() * this.todoList.length);
+    return this.todoList[index];
   }
 
   getTask(id) {
@@ -27,6 +65,7 @@ class Task {
     const task = this.getTask(id);
     if (task) {
       task.status = 'completed';
+      task.complete = true;
     }
     return task;
   }
--- tasks/task-client.js
+++ tasks/task-client.js
@@ -11,19 +11,23 @@ const {
 const socket = io('http://localhost:3000/caps');
 
 socket.on('task-created', (task) => handleTaskCreated(socket, task));
-socket.on('task-completed', handleTaskCompleted);
+socket.on('task-completed', (task) => handleTaskCompleted(socket, task));
 socket.on('task-deleted', handleTaskDeleted);
 
 // Emit a create task event at intervals
 setInterval(() => {
 
   socket.emit('create-task');
-}, 10000);
+}, 4000);
+
 
 // Simulate completing and deleting tasks
 setTimeout(() => {
-  socket.emit('complete-task', { id: 'some-task-id' });
-  socket.emit('delete-task', { id: 'some-task-id' });
-}, 20000);
+  const task = { id: 'some-task-id' };
+  socket.emit('task-completed', task);
+  socket.emit('task-deleted', task);
+}, 6000);
+
+
 
 module.exports ={};
